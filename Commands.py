72926db0103b332e6bb621b8323c2ef0921c5602
Santiago Restrepo-Castillo <santiagorestrepocastillo@outlook.com>
2021-02-10 11:37:55 -0600

    Add files via upload

---
+++ Commands.py
@@ -0,0 +1,20 @@
+    flag10mer = True
+
+    if flag10mer:
+        p20.transfer(1, R1['WELL1'], MISC['OUT'])
+        p20.transfer(1, R1['WELL2'], MISC['OUT'])
+        p20.transfer(1, R2['WELL3'], MISC['OUT'])
+        p20.transfer(1, R2['WELL4'], MISC['OUT'])
+        p20.transfer(1, MISC['A1'], MISC['OUT']) # Backbone
+        p20.transfer(1, MISC['A2'], MISC['OUT']) # Buffer
+        p20.transfer(3.25, MISC['A3'], MISC['OUT']) # Water
+    else:
+        p20.transfer(1, R1['WELL1'], MISC['OUT'])
+        p20.transfer(1, R1['WELL2'], MISC['OUT'])
+        p20.transfer(1, R2['WELL3'], MISC['OUT'])
+        p20.transfer(1, R2['WELL4'], MISC['OUT'])
+        p20.transfer(1, R3['WELL5'], MISC['OUT'])
+        p20.transfer(1, R3['WELL6'], MISC['OUT'])
+        p20.transfer(1, MISC['A1'], MISC['OUT']) # Backbone
+        p20.transfer(1, MISC['A2'], MISC['OUT']) # Buffer
+        p20.transfer(1.25, MISC['A3'], MISC['OUT']) # Water
